a08da9a2b80f633e83be3accd8a66fccc1512bc3
afar22 <aarzuzarueda9@gmail.com>
2026-05-26 20:20:27 -0500

    version 1 del proyecto venturi

---
+++ Config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("BASE_URL", "http://localhost/CH4/public/");
+define("APP_NAME", "CH4");
+define('DB_SERVIDOR', '');
+define('DB_USER', '');
+define('DB_CLAVE', '');
+define('DB_NOMBRE', '');
+
